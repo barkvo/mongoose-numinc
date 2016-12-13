397b43153a4cf1aebdec669e9398fdebaea1b653
Дмитрий <>
2016-12-13 16:36:34 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+module.exports = function(schema, options) {
+	var options = options || {};
+
+	var defaultName = schema.options.collection;
+
+	var fieldName = options.fieldName || 'counter',
+		counterName = options.counterName || defaultName + 'Counter',
+		collectionName = options.collectionName || 'counters';
+
+	var counterSchema = new Schema({ name : String, counter : Number });
+	var CounterModel = mongoose.model('counters', counterSchema);
+
+	schema.pre('save', function (next) {
+		if (!this.isNew) {
+			return next();
+		}
+
+		var self = this;
+
+		CounterModel.findOneAndUpdate(
+			{ name : counterName }, 
+			{ 
+				$inc : { counter : 1 }
+			},
+			{ upsert : true },
+			function(err, counter) {
+				if (err) return next(err);
+
+				self.set(fieldName, counter.counter);
+
+				next();
+			}
+		);
+	});
+}
